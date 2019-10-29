94dced5d5e240aa817184681bb2ca1473d34e05f
GHUTI <yannick.remy16@gmail.com>
2019-10-29 12:05:27 +0100

    Ex 6

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
 $(function() {
   $("#navbarDropdown").click(function(){
     $(".dropdown-menu").toggle();
-  })
+  });
 
   $(".nav-link").removeClass("disabled");
 
   $("#search").keyup(function(){
     $("#searchResult").html($("#search").val())
-  })
+  });
 
   $("#btn").click(function(){
-    console.log("ok");
     $("#blop").text("Web Developer " + Math.floor(Math.random()*10));
-  })
+  });
 
   $(".list-group-item").click(function() {
     $(this).addClass("active").siblings().removeClass("active");
+  });
+
+  $("#playVideo").click(function (){
+    $('#video').trigger("play")
   })
+
+
 });
